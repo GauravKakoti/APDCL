601ef111d4a3eb184c2658d9cc201b9fce826a2b
draxxie <88279586+draxxie@users.noreply.github.com>
2022-08-12 16:39:20 +0530

    Update App.js

--- apdcl-map/src/App.js
+++ apdcl-map/src/App.js
@@ -4,6 +4,36 @@ import * as tt from '@tomtom-international/web-sdk-maps';
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
 import data from './data/data';
 
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+
+function AlertDismissible() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <Alert show={show} variant="success">
+        <Alert.Heading>How's it going?!</Alert.Heading>
+        <p>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+          lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+          fermentum.
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-success">
+            Close me y'all!
+          </Button>
+        </div>
+      </Alert>
+
+      {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+    </>
+  );
+}
+
+render(<AlertDismissible />);
+
 function App() {
   const mapElement = useRef();
   const [map, setMap] = useState({});
